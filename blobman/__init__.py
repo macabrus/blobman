b7698630394a66b3719f1a97df24fb4f540d41a6
Bernard Crnković <bernard.crnkovic@icloud.com>
2024-03-22 00:04:46 +0100

    wip

--- blobman/__init__.py
+++ blobman/__init__.py
@@ -1,17 +1,15 @@
-import datetime
+from getpass import getpass
 import glob
 import itertools
 import json
 import os
 from pathlib import Path
 import random
-import textwrap
 from attr import field
 import cattrs
 import click
 from invoke import run
 from attrs import define
-from cattrs import register_structure_hook, register_unstructure_hook
 
 
 def _git_root(start=os.getcwd()):
@@ -20,13 +18,15 @@ def _git_root(start=os.getcwd()):
         if p == p.root:
             return None
         p = p.parent
-    return str(p)
+    return p
 
 
 GIT_ROOT = Path(_git_root())
+GITIGNORE_PATH = GIT_ROOT / '.gitignore'
 BLOBMAN_PATH = GIT_ROOT / Path('.blobman/')
 CONFIG_PATH = BLOBMAN_PATH / 'config.json'
-LOCK_PATH = BLOBMAN_PATH / 'lock.json'
+VCS_LOCK_PATH = BLOBMAN_PATH / 'lock.json'
+WORKTREE_LOCK_PATH = BLOBMAN_PATH / 'worktree-lock.json'
 LIST_PATH = BLOBMAN_PATH / 'list.txt'
 PASSWORD_PATH = BLOBMAN_PATH / 'password.txt'
 
@@ -40,49 +40,64 @@ def cli():
 def init():
     if not _git_root():
         print('Error: not a git repository')
-    first_run = True
+        return
     if BLOBMAN_PATH.exists():
-        first_run = False
+        print('Error: blobman repository already initialized')
+        return
+
+    c = BlobmanConfig(
+        repository_url=input('Restic Repository: '),
+        repository_password=getpass('Restic Password: '),
+    )
+
     BLOBMAN_PATH.mkdir(parents=True, exist_ok=True)
-    c = BlobmanConfig()
-    if not CONFIG_PATH.exists():
-        _store_config(c)
-    if not LOCK_PATH.exists():
-        store_lock(c)
-    if not PASSWORD_PATH.exists():
-        PASSWORD_PATH.write_text('[REPLACE CONTENT WITH YOUR PASSWORD]')
-    gi = Path('.gitignore')
-    gi.touch(exist_ok=True)
-    _ensure_line('.blobman/password.txt', '.gitignore')
-    if first_run:
-        print(
-            textwrap.dedent(
-                '''
-                Configuration was initialized successfully.
-                To continue, perform following steps:
-                    1. Set "repository_url" field in .blobman/config.json to desired restic remote
-                    2. Set password manually in .blobman/password.txt
-                    3. Re-run 'blob init' to complete initializing restic repository
-                '''
-            )
-        )
-    else:
-        c = _load_config()
-        try:
-            run('restic init', env=_get_env(c))
-        except:
-            pass
-        run(f'git add {LOCK_PATH} {CONFIG_PATH}')
+    CONFIG_PATH.touch(exist_ok=True)
+    WORKTREE_LOCK_PATH.touch(exist_ok=True)
+    VCS_LOCK_PATH.touch(exist_ok=True)
+    PASSWORD_PATH.touch(exist_ok=True)
+
+    GITIGNORE_PATH.touch(exist_ok=True)
+    _ensure_line('.blobman/password.txt', GITIGNORE_PATH)
+    _ensure_line('.blobman/worktree-lock.json', GITIGNORE_PATH)
+    _store_config(c)
+
+    run('restic init', env=_get_env(c))
+    run(f'git add {WORKTREE_LOCK_PATH} {CONFIG_PATH}')
 
 
 @cli.command(help='take a snapshot of local blobs')
-def snapshot():
+@click.option('--dry', 'dry', is_flag=True, default=False, help='show what will be snapshoted')
+def snapshot(dry):
     c = _load_config()
     env = _get_env(c)
-    LIST_PATH.write_text('\n'.join(_ls(c)))
+    files = list(_ls(c))
+    if dry:
+        old_locked_files = [p['file'] for p in c.lock.tracked_files]
+        print('\nTracked files:\n')
+        new_files = []
+        for f in files:
+            print(f)
+            if f not in old_locked_files:
+                new_files.append(f)
+        
+        print('\nNew files:\n')
+        for f in new_files:
+            print(f)
+
+        print('\nRemoved files:\n')
+        for f in old_locked_files:
+            if f not in files:
+                print()
+        return
+    c.lock = LockInfo(
+        tracked_files=[{'file': f} for f in files]
+    )
+    LIST_PATH.write_text(os.linesep.join(files))
     res = run(f'restic backup --files-from-verbatim {str(LIST_PATH)}', env=env)
+    os.remove(LIST_PATH)
     c.snapshot_id = res.stdout.split('\n')[-2].split(' ')[1]
     _store_config(c)
+    _store_lock(c)
 
 
 @cli.command(help='ensure latest blobs are present')
@@ -96,11 +111,18 @@ def checkout():
 @click.option('--diff', is_flag=True, default=False, help='show diff of local & remote blobs')
 def status(diff):
     c = _load_config()
+
+    print('Patterns:')
+    print()
+    for id, pat in c.include_patterns.items():
+        print(id, pat)
+
+    print()
     print('Tracked blobs:')
     print()
     for p in _ls(c, root_dir=_git_root()):
         print(p)
-    
+
     print()
     print('Snapshots:')
     run('restic snapshots', env=_get_env(c))
@@ -128,45 +150,47 @@ def remove(pattern_id: str):
     _store_config(c)
 
 
-register_unstructure_hook(datetime.datetime, datetime.datetime.isoformat)
-register_structure_hook(datetime.datetime, datetime.datetime.fromisoformat)
-
-
 @define(kw_only=True)
 class LockInfo:
-    timestamp: datetime.datetime | None = None
+    snapshot_id: str | None = None
     tracked_files: list[dict[str, str]] = field(factory=list)
 
 
 @define(kw_only=True)
 class BlobmanConfig:
-    repository_url: str = '[REPLACE WITH RESTIC URL]'
-    repository_password: str = None
-    snapshot_id: str | None = None
+    git_root: Path = _git_root()
+    repository_url: str
+    repository_password: str
     include_patterns: dict[str, str] = field(factory=dict)
-    lock: LockInfo = field(factory=LockInfo)
+    vcs_lock: LockInfo = field(factory=LockInfo)
+    worktree_lock: LockInfo = field(factory=LockInfo)
 
 
 def _load_config() -> BlobmanConfig:
-    if not CONFIG_PATH.exists():
-        print('Error: blobman repository not initialized for this git repository')
-        return
-    return cattrs.structure(json.loads(CONFIG_PATH.read_text()), BlobmanConfig)
+    for p in (CONFIG_PATH, VCS_LOCK_PATH, WORKTREE_LOCK_PATH):
+        if not p.exists():
+            print(f'Error: file not found {p}')
+            exit(1)
+    return cattrs.structure(
+        json.loads(CONFIG_PATH.read_text()) | {
+            'git_root': _git_root(),
+            'repository_password': PASSWORD_PATH.read_text(),
+            'lock': json.loads(VCS_LOCK_PATH.read_text()),
+            'worktree_lock': json.loads(WORKTREE_LOCK_PATH.read_text())
+        },
+        BlobmanConfig
+    )
 
 
 def _store_config(config: BlobmanConfig) -> None:
-    j = cattrs.unstructure(config)
-    del j['repository_password']
-    del j['lock']
-    CONFIG_PATH.write_text(json.dumps(j, indent=4))
+    o = cattrs.unstructure(config)
+    PASSWORD_PATH.write_text(json.dumps(o.pop('repository_password'), indent=4))
+    VCS_LOCK_PATH.write_text(json.dumps(o.pop('vcs_lock'), indent=4))
+    WORKTREE_LOCK_PATH.write_text(json.dumps(o.pop('worktree_lock'), indent=4))
+    CONFIG_PATH.write_text(json.dumps(o, indent=4))
 
 
-def store_lock(config: BlobmanConfig) -> None:
-    j = cattrs.unstructure(config.lock)
-    LOCK_PATH.write_text(json.dumps(j, indent=4))
-
-
-def _get_env(c):
+def _get_env(c: BlobmanConfig):
     return {
         'PWD': _git_root(),
         'RESTIC_REPOSITORY': c.repository_url,
@@ -193,3 +217,13 @@ def _ensure_line(line, file):
         return
     with Path(file).open('a') as f:
         f.write(str(line) + os.linesep)
+
+
+# get a list of filest currently managed by blobman
+def _list_blob_files():
+    ...
+
+
+# get a list of files tracked by git
+def _list_git_files():
+    return run('git ls-tree -r HEAD --name-only').stdout.split(os.linesep)
